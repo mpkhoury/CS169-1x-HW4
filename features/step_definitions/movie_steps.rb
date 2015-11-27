4570f4ad7119132f976693b8ff345eeeb94585e4
Marcelo Picolo Khoury <mpkhoury@gmail.com>
2015-11-27 11:10:39 +0000

    Partial commit

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -10,3 +10,20 @@ end
 Then /^the director of "(.*?)" should be "(.*?)"$/ do |film, director|
   
 end
+
+Given /^I am on the details page for "(.*?)"$/ do |film|
+  
+end
+
+When /^I follow "Find Movies With Same Director"$/ do
+  
+end
+
+Then /^I should be on the (.*?) page for "(.*?)"$/ do |page_name, film|
+    current_path = URI.parse(current_url).path
+    if current_path.respond_to? :should
+      current_path.should == path_to(page_name)
+    else
+      assert_equal path_to(page_name), current_path
+    end
+end
